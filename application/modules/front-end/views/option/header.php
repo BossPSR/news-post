e43aecc4a8d31ac552e57214f88d9fd3d1a04743
worapongsrisawan <famnoii2550@gmail.com>
2020-04-07 14:27:31 +0700

    default file

--- application/modules/front-end/views/option/header.php
+++ application/modules/front-end/views/option/header.php
@@ -70,8 +70,29 @@
 				</form>
 				<div class="col_full nobottommargin">
 					<button class="button button-rounded t400 nomargin" id="send_login">เข้าสู่ระบบ</button>
-					<a href="#" class="text-muted fright">ลืมรหัสผ่าน</a>
+					<a class="text-muted fright" data-toggle="modal" data-target="#exampleModal">ลืมรหัสผ่าน</a>
+					<!-- Modal -->
+					<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body">
+									...
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+									<button type="button" class="btn btn-primary">Save changes</button>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
+
 
 
 			</div>
