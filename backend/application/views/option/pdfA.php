b5ef19ebf26dc97a45516f4f05a93cfbb2b5f8df
miki <mikiboy004>
2020-04-20 18:06:06 +0700

    order pdf 20/4/2563

---
+++ backend/application/views/option/pdfA.php
@@ -0,0 +1,15 @@
+<hr>
+<div class="form-group travel-date-group" style="text-align: left;font-size:16px;">
+    <b>วันที่ลงโฆษณา</b>
+    <input type="date" name="date" class="form-control tleft default" min="<?php echo date('Y-m-d')?>"  >
+</div>
+<div class="form-group travel-date-group" style="text-align: left;font-size:16px;">
+    <b>หน้าที่เลือกคือ []</b>
+</div>
+<div class="row">
+    <div class="col-lg-5 col-md-5 col-sm-12">
+        <div class="form-group" style="text-align: left;font-size:16px;">
+            <input id="inputfile" name="file_name" type="file" accept="application/pdf"  data-show-preview="false">
+        </div>
+    </div>
+</div>
